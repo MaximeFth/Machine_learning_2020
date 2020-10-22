7414dcd408b667bac1daa2fcb95a7b0a2622efd7
maxime Fellrath <maxime.fellrath@gmail.com>
2020-10-22 11:54:53 +0200

    new standardize function

--- Project1Martin/costs.py
+++ Project1Martin/costs.py
@@ -2,7 +2,16 @@
 """Function used to compute the loss."""
 import numpy as np
 def compute_loss(y, tx, w):
-
-    return np.square(np.subtract(y,tx@w)).mean()
+    """
+    compute mse cost
+    
+    :param y: labels
+    :param tx: features
+    :param w: weights
+    
+    :return mse: mean square loss 
+    """
+    mse = np.square(np.subtract(y,tx@w)).mean()
+    return mse
 
 
